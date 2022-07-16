4c8cd0569e17e4831e5b2b2fe9fc7e89f8b30b9a
saindi <saindi777@gmail.com>
2022-07-16 22:17:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from typing import List
+import sys
+import time
+
+
+def find_uniq(dataset: List[int]) -> int:
+    dict_dataset = dict.fromkeys(dataset, 0)
+
+    for item in dataset:
+        dict_dataset[item] += 1
+
+    for key, value in dict_dataset.items():
+        if value == 1:
+            print(f"доп память сайз: {str(sys.getsizeof(dict_dataset))}")
+            return key
+
+
+if __name__ == "__main__":
+    with open("test1.txt", "r", encoding="utf-8") as file:
+        test_list = list(map(int, file.read().split()))
+
+    start_time = time.time()
+    print(f"answer = {find_uniq(test_list)}")
+    print("--- %s seconds ---\n" % (time.time() - start_time))
