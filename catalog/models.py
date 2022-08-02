8be55403af93fbc00da3b54e3ff8511ca7db3ee6
saindi <saindi777@gmail.com>
2022-08-02 13:22:09 +0300

    add __str__ to all models

--- catalog/models.py
+++ catalog/models.py
@@ -7,6 +7,9 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
+    def __str__(self):
+        return self.slug
+
 
 class Product(models.Model):
     slug = models.SlugField(primary_key=True)
@@ -14,7 +17,10 @@ class Product(models.Model):
     available = models.BooleanField(default=False)
     description = models.TextField()
     price = models.FloatField(validators=[MinValueValidator(limit_value=0,
-                                                              message='Цена не мможет быть отрицательная!')])
+                                                            message='Цена не мможет быть отрицательная!')])
     amount = models.IntegerField()
     img = models.ImageField(upload_to='img')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.slug
--- user/models.py
+++ user/models.py
@@ -8,3 +8,6 @@ class User(models.Model):
     username = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
