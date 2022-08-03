a242ecfe8428e80dca1dcf027294205915d29d61
saindi <saindi777@gmail.com>
2022-08-04 00:14:36 +0300

    fix product url

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,5 @@ from catalog.views import catalog_page, product_page, catalog_page_with_category
 urlpatterns = [
     path('', catalog_page, name='catalog_url'),
     path('<slug:category_slug>/', catalog_page_with_category, name="catalog_page_with_category_url"),
-    path('product/<slug:product_slug>/', product_page),
+    path('product/<slug:product_slug>/', product_page, name="product_url"),
 ]
