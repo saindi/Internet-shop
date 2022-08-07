0fe28a9e29e0e1ab163ee4801a5c8a40aea660e1
saindi <saindi777@gmail.com>
2022-08-07 12:43:01 +0300

    make homework_10

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from user.views import user_view
+from user.views import user_view, signin_view, signup_view, logout_view, deactivation_user_view
 
 urlpatterns = [
     path('user/', user_view, name="user_url"),
+    path('user/deactivation/', deactivation_user_view, name="deactivation_user_url"),
+    path('sign-in/', signin_view, name="signin_url"),
+    path('sign-up/', signup_view, name="signup_url"),
+    path('logout/', logout_view, name="logout_url"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,69 @@
-from django.http import HttpRequest, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
 
 
 def user_view(request: HttpRequest) -> HttpResponse:
+    if request.user.is_anonymous:
+        return HttpResponseRedirect(reverse_lazy('signin_url'))
+
     return render(request, 'user/user.html')
+
+
+def signin_view(request: HttpRequest) -> HttpResponse:
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse_lazy('user_url'))
+
+    if request.method == "POST":
+        user = authenticate(username=request.POST['username'],
+                            password=request.POST['password'])
+
+        if user is None:
+            return HttpResponseRedirect(reverse_lazy('signin_url'))
+
+        login(request, user)
+
+        return HttpResponseRedirect(reverse_lazy('home_url'))
+
+    return render(request, 'user/signin.html')
+
+
+def signup_view(request: HttpRequest) -> HttpResponse:
+    #
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse_lazy('user_url'))
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2 and len(User.objects.filter(username=username)) == 0:
+            User.objects.create_user(username=username,
+                                     password=password1)
+            return HttpResponseRedirect(reverse_lazy('signin_url'))
+
+        return HttpResponseRedirect(reverse_lazy('signup_url'))
+
+    return render(request, 'user/signup.html')
+
+
+def logout_view(request: HttpRequest) -> HttpResponse:
+    logout(request)
+    return HttpResponseRedirect(reverse_lazy('home_url'))
+
+
+def deactivation_user_view(request: HttpRequest) -> HttpResponse:
+    if request.user.is_anonymous:
+        return HttpResponseRedirect(reverse_lazy('home_url'))
+
+    user = User.objects.get(username=request.user.username)
+
+    user.is_active = False
+    user.save()
+
+    logout(request)
+
+    return HttpResponseRedirect(reverse_lazy('home_url'))
