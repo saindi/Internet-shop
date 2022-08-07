1a19e208d156783c3ba03d364b7427222695954f
saindi <saindi777@gmail.com>
2022-08-07 12:55:41 +0300

    add name home url

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from catalog.views import home_view, catalog_view, product_view, category_view
 
 urlpatterns = [
-    path('', home_view),
+    path('', home_view, name='home_url'),
     path('catalog/', catalog_view, name='catalog_url'),
     path('catalog/<slug:category_slug>/', category_view, name="category_url"),
     path('catalog/<slug:category_slug>/<slug:product_slug>/', product_view, name="product_url"),
