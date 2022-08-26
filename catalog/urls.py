a730c1a829bcef4e5cb97a578d3201eabf6b950d
saindi <saindi777@gmail.com>
2022-08-26 17:44:21 +0300

    fix urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,7 +9,7 @@ from catalog.views import (
     ProductDetailView,
     ProductUpdateView,
     ProductDeleteView,
-    ProductCreateView
+    ProductCreateView,
 )
 
 
@@ -19,13 +19,13 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home_url'),
 
     path('category/', CategoryListView.as_view(), name='category_list_url'),
+    path('category/create/', CategoryCreateView.as_view(), name="category_create_url"),
     path('category/<slug:category_slug>/update/', CategoryUpdateView.as_view(), name="category_update_url"),
     path('category/<slug:category_slug>/delete/', CategoryDeleteView.as_view(), name="category_delete_url"),
-    path('category/create/', CategoryCreateView.as_view(), name="category_create_url"),
 
+    path('product/create/', ProductCreateView.as_view(), name="product_create_url"),
     path('category/<slug:category_slug>/', ProductListView.as_view(), name="product_list_url"),
     path('product/<slug:product_slug>/', ProductDetailView.as_view(), name="product_detail_url"),
     path('product/<slug:product_slug>/update/', ProductUpdateView.as_view(), name="product_update_url"),
     path('product/<slug:product_slug>/delete/', ProductDeleteView.as_view(), name="product_delete_url"),
-    path('product/create/', ProductCreateView.as_view(), name="product_create_url"),
 ]
